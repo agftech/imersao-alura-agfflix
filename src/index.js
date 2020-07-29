295952e61640ca262d2ba21b8dfde8762ce54df3
agftech <50276045+agftech@users.noreply.github.com>
2020-07-28 22:50:29 -0300

    #Second Class - Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import "./reset.css";
 
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Page404 from "./pages/404";
+import CadastroVideo from "./pages/cadastro/Video";
+import CadastroCategoria from "./pages/cadastro/Categoria";
+
+// Desafio master blaster na descrição
+// Colocar um jogo nessa página: https://www.youtube.com/watch?v=jOAU81jdi-c :)
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+	<BrowserRouter>
+		<Switch>
+			<Route path='/' component={Home} exact />
+			<Route path='/cadastro/categoria' component={CadastroCategoria} />
+			<Route path='/cadastro/video' component={CadastroVideo} />
+			<Route component={Page404} />
+		</Switch>
+	</BrowserRouter>,
 	document.getElementById("root")
 );
